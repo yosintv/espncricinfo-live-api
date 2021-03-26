795234a7f44777eaa0d9af46bd59ba9ab7ad61ac
Gourab Das <vicky.slg100@gmail.com>
2021-03-26 15:22:02 +0530

    cross

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+var cors = require('cors');
+const PORT = process.env.port||'8080';
 const app = express();
 app.use(express.static("public"));
-const PORT = process.env.port||'8080';
+app.use(cors());
 
   app.use(require('./router/home'));
   app.use(require('./router/live'));
   
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 8080, () => console.log("Server is running..."));
